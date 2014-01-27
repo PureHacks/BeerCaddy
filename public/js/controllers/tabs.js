f2d6c609c22a5e28aaeea504fc9f903cec9d5a21
Michael Mrowetz <michaelmrowetz@gmail.com>
2014-01-26 20:22:12 -0500

    beers owed added

--- public/js/controllers/tabs.js
+++ public/js/controllers/tabs.js
@@ -10,6 +10,7 @@ angular.module('mean.tabs').controller('TabsController', ['$scope', '$routeParam
             //todo: implement floor
             case "eighthFloor" : return tab.floor == 8;
             case "ninthFloor" : return tab.floor == 9;
+            case "owsBeer" : return tab.tab < 0;
             case "getBeer" : return tab.tab > 0;
             default : return true;
         }
